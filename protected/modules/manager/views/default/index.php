3f38f67fd4e1859af2338f339e3af5dc88468617
huynh duong quang <duongqhuynh@gmail.com>
2014-06-12 10:54:34 +0700

    fix css file

--- protected/modules/manager/views/default/index.php
+++ protected/modules/manager/views/default/index.php
@@ -7,17 +7,17 @@ $this->breadcrumbs=array(
 ?>
 <h3> Manage page for managing hospital's information</h3>
 <?php 
-		echo CHtml::link("- Manager questions",Yii::app()->baseUrl."/manager/info/default/question",array("style"=>"color: darkblue;"));
+		echo CHtml::link("> Manager Questions",Yii::app()->baseUrl."/manager/info/default/question",array("style"=>"color: darkblue;"));
 		echo "<p></p>";
-		echo CHtml::link("- Manager events",Yii::app()->baseUrl."/manager/info/default/event",array("style"=>"color: darkblue;"));
+		echo CHtml::link("> Manager Events",Yii::app()->baseUrl."/manager/info/default/event",array("style"=>"color: darkblue;"));
 		echo "<p></p>";
-		echo CHtml::link("- Manager notices",Yii::app()->baseUrl."/manager/info/default/notice",array("style"=>"color: darkblue;"));
+		echo CHtml::link("> Manager Notices",Yii::app()->baseUrl."/manager/info/default/notice",array("style"=>"color: darkblue;"));
 		echo "<p></p>";
-		echo CHtml::link("- Manager appointment",Yii::app()->baseUrl."/manager/info/default/appointment",array("style"=>"color: darkblue;"));
+		echo CHtml::link("> Manager Appointment",Yii::app()->baseUrl."/manager/info/default/appointment",array("style"=>"color: darkblue; text-decoration: bold;"));
 		echo "<p></p>";
-// 		echo CHtml::link("- Manager infos",Yii::app()->baseUrl."/manager/info/default/admin",array("style"=>"color: darkblue;"));
+// 		echo CHtml::link("> Manager infos",Yii::app()->baseUrl."/manager/info/default/admin",array("style"=>"color: darkblue;"));
 // 		echo "<p></p>";
-		echo CHtml::link("- Manager users",Yii::app()->baseUrl."/manager/user/default/admin",array("style"=>"color: darkblue;"));
+		echo CHtml::link("> Manager Users",Yii::app()->baseUrl."/manager/user/default/admin",array("style"=>"color: darkblue;"));
 		echo "<p></p>";
-		echo CHtml::link("- Log events",Yii::app()->baseUrl."/manager/logevent/default/admin",array("style"=>"color: darkblue;"));
+		echo CHtml::link("> Log events",Yii::app()->baseUrl."/manager/logevent/default/admin",array("style"=>"color: darkblue;"));
 ?>
