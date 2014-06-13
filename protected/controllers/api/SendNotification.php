eee338ce7786273f1090a55a4946c4d6145868e9
huynh duong quang <duongqhuynh@gmail.com>
2014-06-13 15:32:58 +0700

    ưer

--- protected/controllers/api/SendNotification.php
+++ protected/controllers/api/SendNotification.php
@@ -20,6 +20,10 @@ class SendNotification {
 		$message = 'testing';
 		$gcm = Yii::app()->gcm;
 		$tokens = array_unique($tokens);
+		echo "<pre>";
+		print_r($tokens);
+		echo "</pre>";
+		die();
 		$gcm->sendMulti($tokens, $message, array('extra' => 'multi devices ', 'title'=>$title, 'value' => $content, 'info_type_id'=>$info_type_id, 'id'=>$id), array( 'delayWhileIdle' => true ));
 	}
 }
