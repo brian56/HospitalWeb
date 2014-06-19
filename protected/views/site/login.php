0cbf9f3ac1aa97b2217847b8a31461ada443c878
huynh duong quang <duongqhuynh@gmail.com>
2014-06-19 13:37:10 +0700

    add language picker, fix Login button

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -7,8 +7,13 @@ $this->pageTitle=Yii::app()->name . ' - '.Yii::t('strings','Login');
 $this->breadcrumbs=array(
 	Yii::t('strings','Login'),
 );
-?>
-
+$this->widget('ext.LangPick.ELangPick', array(
+	    //'excludeFromList' => array('pl', 'en'), // list of languages to exclude from list
+	    'pickerType' => 'dropdown',              // buttons, links, dropdown
+	    //'linksSeparator' => '<b> | </b>',   // if picker type is set to 'links'
+	    'buttonsSize' => 'small',                // mini, small, large
+	    'buttonsColor' => 'primary',            // primary, info, success, warning, danger, inverse
+	)); ?>
 <h3><?php echo Yii::t('strings','Login');?></h3>
 
 <p><?php echo Yii::t('strings','Please fill out the following form with your login credentials:');?></p>
@@ -44,7 +49,15 @@ $this->breadcrumbs=array(
 	</div>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton(Yii::t('strings','Login')); ?>
+		<?php 
+		$this->widget(
+				'booster.widgets.TbButton',
+				array(
+						'label' => Yii::t('strings','Login'),
+						'context' => 'primary',
+						'buttonType' => 'submit',
+				)
+		);?>
 	</div>
 
 <?php $this->endWidget(); ?>
